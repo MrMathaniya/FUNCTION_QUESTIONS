3998cc3268523e2ac09ae4ee07d4fd32c802341e
Vansh Mathaniya <108232361+MrMathaniya@users.noreply.github.com>
2026-01-30 21:03:14 +0530

    Update Q19.py

--- Q19.py
+++ Q19.py
@@ -1,10 +1,8 @@
 def power(a,n):
-    result=1
-    for i in range(n):
-       result*=a
-    return result
+   return a**n
 
 a=int(input("Enter the base number: "))
 n=int(input("Enter the power: "))
 print(power(a,n))
+
 
